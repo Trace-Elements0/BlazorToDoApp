86d4a5da67b9491c9e5ae66a83c21f657f09c3d8
Jorge Sanchez <62453774+Trace-Elements0@users.noreply.github.com>
2020-08-31 00:35:38 -0400

    initial commit

---
+++ InputNumbers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList
+{
+    public class InputNumbers
+    {
+        public int FirstOperand{ get; set; }
+        public int SecondOperand{ get; set; }
+        public int Sum { get; set; }
+    }
+}
